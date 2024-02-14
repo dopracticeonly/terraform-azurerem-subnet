ca361e2ff4e0e30e714603c24cbf36783489809b
dopracticeonly <138019649+dopracticeonly@users.noreply.github.com>
2024-02-14 13:23:29 +0530

    Add files via upload

---
+++ subnet.tf
@@ -0,0 +1,67 @@
+module "subnet" {
+  source  = "git::https://github.com//dopracticeonly//azurerem-terraform-subnet"
+  version = "2.3.0"
+
+subnets = {
+    mgnt_subnet = {
+      subnet_name           = "snet-management"
+      subnet_address_prefix = ["10.1.2.0/24"]
+
+      delegation = {
+        name = "testdelegation"
+        service_delegation = {
+          name    = "Microsoft.ContainerInstance/containerGroups"
+          actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+        }
+      }
+
+      nsg_inbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["weballow", "100", "Inbound", "Allow", "Tcp", "80", "*", "0.0.0.0/0"],
+        ["weballow1", "101", "Inbound", "Allow", "", "443", "*", ""],
+        ["weballow2", "102", "Inbound", "Allow", "Tcp", "8080-8090", "*", ""],
+      ]
+
+      nsg_outbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["ntp_out", "103", "Outbound", "Allow", "Udp", "123", "", "0.0.0.0/0"],
+      ]
+    }
+
+    dmz_subnet = {
+      subnet_name           = "snet-appgateway"
+      subnet_address_prefix = ["10.1.3.0/24"]
+      service_endpoints     = ["Microsoft.Storage"]
+
+      nsg_inbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+        ["weballow", "200", "Inbound", "Allow", "Tcp", "80", "*", ""],
+        ["weballow1", "201", "Inbound", "Allow", "Tcp", "443", "AzureLoadBalancer", ""],
+        ["weballow2", "202", "Inbound", "Allow", "Tcp", "9090", "VirtualNetwork", ""],
+      ]
+
+      nsg_outbound_rules = [
+        # [name, priority, direction, access, protocol, destination_port_range, source_address_prefix, destination_address_prefix]
+        # To use defaults, use "" without adding any values.
+      ]
+    }
+
+    pvt_subnet = {
+      subnet_name           = "snet-pvt"
+      subnet_address_prefix = ["10.1.4.0/24"]
+      service_endpoints     = ["Microsoft.Storage"]
+    }
+  }
+
+  # Adding TAG's to your Azure resources (Required)
+  tags = {
+    ProjectName  = "demo-internal"
+    Env          = "dev"
+    Owner        = "user@example.com"
+    BusinessUnit = "CORP"
+    ServiceClass = "Gold"
+  }
+}
